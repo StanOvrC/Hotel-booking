64c36defdd278a532ef482cf5d045fd407b50755
Stan <sls2003sls@gmail.com>
2025-11-03 15:09:32 +0300

    Add Booking entity

---
+++ src/main/java/com/rsoi/hotel_booking/entity/Booking.java
@@ -0,0 +1,42 @@
+package com.rsoi.hotel_booking.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @Column(name = "check_in_date")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out_date")
+    private LocalDate checkOutDate;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+    public enum Status {
+        CONFIRMED, CANCELLED, PENDING
+    }
+}

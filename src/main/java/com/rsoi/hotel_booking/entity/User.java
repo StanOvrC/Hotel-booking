9c678d7ad17e34313f505742a34b765a33c95e2e
Stan <sls2003sls@gmail.com>
2025-11-03 14:22:42 +0300

    Add User entity

---
+++ src/main/java/com/rsoi/hotel_booking/entity/User.java
@@ -0,0 +1,31 @@
+package com.rsoi.hotel_booking.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        GUEST, MANAGER, ADMIN
+    }
+}
